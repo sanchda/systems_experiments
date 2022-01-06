c15183d332c177fcce7209b5a14b9b898ce83351
sanchda <sanchda@users.noreply.github.com>
2022-01-06 05:58:58 +0000

    Make reporting automatic

--- threadlocal/test.c
+++ threadlocal/test.c
@@ -3,10 +3,11 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void helperfun() {
   static __thread char a = 0;
-  return;
 }
 
 void *worker(void *arg) {
@@ -16,35 +17,40 @@ void *worker(void *arg) {
 void *lazy(void *arg) {
 }
 
+void print_stats() {
+  usleep(10000);
+  char proc_loc[1024] = {0};
+  snprintf(proc_loc, sizeof(proc_loc), "/proc/%d/status", getpid());
+  char *commands[] = {"grep", "-i", "rss", proc_loc, NULL};
+  pid_t pid = fork();
+  if (!pid)
+    execvp(commands[0], commands);
+  waitpid(pid, 0, 0);
+}
+
 #define NUM_THREADS 10000
 #define INSTRUCTIONS "`grep -i rss /proc/%d/status`"
 int main() {
-  printf("Doing a run and joining %d threads. "INSTRUCTIONS"\n", NUM_THREADS, getpid());
+  printf("Doing a run and joining %d threads\n", NUM_THREADS);
   pthread_t tids[NUM_THREADS] = {0};
   for(int i=0; i<NUM_THREADS; i++) pthread_create(&tids[i], NULL, worker, NULL);
   for(int i=0; i<NUM_THREADS; i++) pthread_join(tids[i], NULL);
-  char buf[100] = {0};
-  printf("Hit enter to continue\n");
-  read(0, buf, 1);
+  print_stats();
 
-  printf("doing a run and not joining %d lazy threads. "INSTRUCTIONS"\n", NUM_THREADS, getpid());
+  printf("Doing a lazy run and NOT joining %d threads\n", NUM_THREADS);
   for(int i=0; i<NUM_THREADS; i++) pthread_create(&tids[i], NULL, lazy, NULL);
-  printf("Hit enter to continue\n");
-  read(0, buf, 1);
+  print_stats();
 
-  printf("joining %d lazy threads. "INSTRUCTIONS"\n", NUM_THREADS, getpid());
+  printf("joining threads\n");
   for(int i=0; i<NUM_THREADS; i++) pthread_join(tids[i], NULL);
-  printf("Hit enter to continue\n");
-  read(0, buf, 1);
+  print_stats();
 
-  printf("Doing a run and NOT joining %d threads. "INSTRUCTIONS"\n", NUM_THREADS, getpid());
+  printf("Doing a lazy run and NOT joining %d threads\n", NUM_THREADS);
   for(int i=0; i<NUM_THREADS; i++) pthread_create(&tids[i], NULL, worker, NULL);
-  printf("Hit enter to continue\n");
-  read(0, buf, 1);
+  print_stats();
 
-  printf("joining %d threads. "INSTRUCTIONS"\n", NUM_THREADS, getpid());
+  printf("joining threads\n");
   for(int i=0; i<NUM_THREADS; i++) pthread_join(tids[i], NULL);
-  printf("Hit enter to continue\n");
-  read(0, buf, 1);
+  print_stats();
   return 0;
 }
