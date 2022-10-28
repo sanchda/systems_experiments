6aa21a7e9104a68b42ccb35db547676b7670c826
sanchda <sanchda@users.noreply.github.com>
2022-10-28 01:33:23 -0500

    Update

--- getpwuid_r/Readme.md
+++ getpwuid_r/Readme.md
@@ -2,4 +2,5 @@ getpwuid_r
 ===
 
 For diagnostic purposes, this is a small wrapper over getpwuid_r that avoids
-heap allocations.
+heap allocations.  Pass it a combination of UIDs and usernames, it will loop
+over them infinitely and get the opposite (UID->user, user->UID).
--- getpwuid_r/test.c
+++ getpwuid_r/test.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <stdbool.h>
+#include <stdlib.h> //
 #include <string.h> // memset
 #include <sys/types.h>
 #include <pwd.h> //getpwnam_r, getpwuid_r
@@ -7,7 +8,7 @@
 
 
 #define BUF_SZ 4096
-bool user_to_id(const char *user, uid_t *id) {
+bool uid_from_user(const char *user, uid_t *id) {
   int stashed_errno;
   if (!user || !*user)
     return false;
@@ -37,7 +38,7 @@ bool user_to_id(const char *user, uid_t *id) {
   return true;
 }
 
-bool check_id(uid_t id) {
+const char* user_from_uid(uid_t id) {
   int stashed_errno;
   struct passwd pwd;
   struct passwd *result;
@@ -53,35 +54,86 @@ bool check_id(uid_t id) {
     write(1, err_msg, sizeof(err_msg));
     write(1, strerror(stashed_errno), strlen(strerror(stashed_errno)));
     write(1, "\n", 1);
-    return false;
+    return NULL;
   }
+  return pwd.pw_name;
+}
 
-  // If we're here, good job we succeeded :thumbsup:
-  static char success_msg[] = "Successfully called getpwuid_r()\n";
-  write(1, success_msg, sizeof(success_msg));
-  return true;
+bool is_number(const char *str) {
+  if (!str || !*str) return false;
+  while (*str <= '9' && *str >= '0') str++;
+  return *str == '\0';
+}
+
+void write_number(long num) {
+  static char buf[sizeof("18446744073709551616")]; // biggest uint64_t
+  memset(buf, '\0', sizeof(buf));
+  size_t sz = sizeof(buf) - 1;
+  if (num == 0)
+    buf[sz--] = '0';
+  else while (num) {
+    buf[sz--] = '0' + (num % 10);
+    num /= 10;
+  }
+  write(STDOUT_FILENO, &buf[sz], sizeof(buf)-sz);
+}
+
+void process_argument(const char *str) {
+  uid_t uid = 0;
+  const char *user = NULL;
+  if (is_number(str)) {
+    uid = strtol(str, NULL, 10);
+    const char msg[] = "Running test on UID: ";
+    write(1, msg, sizeof(msg));
+    write_number(uid);
+    write(1, "\n", 1);
+    if (!(user = user_from_uid(uid))) {
+      const char msg[] = "Failed to get user\n";
+      write(1, msg, sizeof(msg));
+      return;
+    } else {
+      const char pre_msg[] = "Got user: `";
+      const char post_msg[] = "`\n";
+      write(1, pre_msg, sizeof(pre_msg));
+      write(1, user, strlen(user));
+      write(1, post_msg, sizeof(post_msg));
+    }
+  } else {
+    user = str;
+    const char pre_msg[] = "Running test on user `";
+    const char post_msg[] = "`\n";
+    write(1, pre_msg, sizeof(pre_msg));
+    write(1, user, strlen(user));
+    write(1, post_msg, sizeof(post_msg));
+
+    // Call getpwuid_r from a wrapper, so we can ensure transient stack storage
+    //for temporaries.
+    if (!uid_from_user(user, &uid)) {
+      const char msg[] = "Failed to get UID\n";
+      write(1, msg, sizeof(msg));
+      return;
+    } else {
+      const char msg[] = "Got UID: ";
+      write(1, msg, sizeof(msg));
+      write_number(uid);
+      write(1, "\n", 1);
+    }
+  }
 }
 
 int main(int n, char **V) {
-  static const char default_user[] = "nobody";
-  const char *user = default_user;
-  uid_t id = -1;
-
-  // If an argument is supplied, treat it as a user
-  if (n > 1)
-    user = V[1];
-
-  static char pre_msg[] = "Running test on user `";
-  static char post_msg[] = "`\n";
-  write(1, pre_msg, sizeof(pre_msg));
-  write(1, user, strlen(user));
-  write(1, post_msg, sizeof(post_msg));
-
-  // Call getpwuid_r from a wrapper, so we can ensure transient stack storage
-  //for temporaries.
-  if (!user_to_id(user, &id))
+  if (n == 1) {
+    const char msg[] = "You've got to give me something to work with here.\n";
+    write(1, msg, sizeof(msg));
     return -1;
-  if (!check_id(id))
-    return -1;
+  }
+
+  // Loop over the input arguments over and over
+  int i = 0;
+  n--;
+  V++;
+  while (true)
+    process_argument(V[i++ % n]);
+
   return 0;
 }
