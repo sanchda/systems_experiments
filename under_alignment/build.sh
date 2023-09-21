b8b8555b9bb2378c2f6127b29c24d38e54a11ec1
sanchda <sanchda@users.noreply.github.com>
2023-09-21 17:56:17 +0000

    Is this a real malloc thing

--- under_alignment/build.sh
+++ under_alignment/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for a in `seq 1 5`; do
+for a in `seq 1 6`; do
   echo
   echo "==================== Testing alignment $((2**a))"
   clang++-13 -std=c++17 test.cpp -fnew-alignment=$((2**a)) -o test
-  ./test
+  MALLOC_ALIGNMENT=$((2**a)) ./test
 done
