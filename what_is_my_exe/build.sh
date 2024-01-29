5f0ea8d7eac96f2cb84b4dc135341336d4e74e03
sanchda <sanchda@users.noreply.github.com>
2024-01-29 13:32:44 +0000

    what_is_my_exe

--- what_is_my_exe/build.sh
+++ what_is_my_exe/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++-10 -std=c++17 test.cpp -o test
+g++-10 -std=c++17 -Os -s -fno-asynchronous-unwind-tables -fno-unwind-tables -flto -fno-rtti -Wl,--gc-sections -Wl,--strip-all test.cpp -o test
--- what_is_my_exe/test.cpp
+++ what_is_my_exe/test.cpp
@@ -4,29 +4,23 @@
 #include <string>
 
 std::string file_from_addr(void* addr) {
-    std::ifstream maps("/proc/self/maps");
-    std::string line;
-    while (std::getline(maps, line)) {
-        std::istringstream iss(line);
-        std::string addr_str;
-        iss >> addr_str;
-        if (addr_str.find('-') != std::string::npos) {
-            std::istringstream addr_iss(addr_str);
-            void* start_addr;
-            void* end_addr;
-            addr_iss >> start_addr;
-            addr_iss.ignore();
-            addr_iss >> end_addr;
-            if (addr >= start_addr && addr <= end_addr) {
-              std::string path;
-              std::string token;
-              while (iss >> token)
-                path = token;
-              return path;
-            }
+  std::ifstream maps("/proc/self/maps");
+  std::string line;
+  while (std::getline(maps, line)) {
+    auto pos = line.find('-');
+    if (pos != std::string::npos) {
+      std::uintptr_t start_addr = std::stoull(line.substr(0, pos), nullptr, 16);
+      std::uintptr_t end_addr = std::stoull(line.substr(pos + 1), nullptr, 16);
+
+      if (reinterpret_cast<std::uintptr_t>(addr) >= start_addr && reinterpret_cast<std::uintptr_t>(addr) < end_addr) {
+        auto last_space = line.rfind(' ');
+        if (last_space != std::string::npos) {
+          return line.substr(last_space + 1);
         }
+      }
     }
-    return "";
+  }
+  return "";
 }
 
 int fingerprint = 0;
