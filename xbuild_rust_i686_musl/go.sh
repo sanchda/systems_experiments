c9d35d26f027a7968debac821203a9c4d3c10f0c
sanchda <sanchda@users.noreply.github.com>
2024-05-22 19:22:43 +0000

    Some stuff

--- xbuild_rust_i686_musl/go.sh
+++ xbuild_rust_i686_musl/go.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Of course, linux/386 is actually i686 support.  Duh.
-# This might help: docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker buildx create --use --name larger_log --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=50000000
-docker buildx build --progress plain --output type=docker -t rust-nightly-alpine:i686 . && \
-  docker run --rm -it -v ./configs:/configs rust-nightly-alpine:i686
+docker buildx create --use --name rust_builder
+docker buildx build \
+  --progress plain \
+  --target final \
+  --output type=docker \
+  -t rust-nightly-alpine:i686 \
+  . && \
+docker run --rm -it \
+  -v ./configs:/configs \
+  rust-nightly-alpine:i686
