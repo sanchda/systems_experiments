7b046db5bc9112422188a48fd715aa0560f12fe9
sanchda <838104+sanchda@users.noreply.github.com>
2024-08-22 18:03:30 +0000

    Update finder

--- wherefore_art_thou_python/finder.py
+++ wherefore_art_thou_python/finder.py
@@ -1,43 +1,26 @@
-# wherefore doesn't mean "where" lol
-# eg python3 finder.py $(echo $PATH | tr ':' ' ') | xargs -n 1 basename | sort | uniq
 import os
 import sys
-import subprocess
 
-def is_python_shebang(line):
-    return line.startswith("#!") and "python" in line.lower()
-
-def is_text_file(filepath):
+def file_launched_with_python(filepath):
+    # The first two bytes of an executable are special, #! indicates it's meant to be "interpreted"
+    # by the specified program; if that program is a Python, then mark that file.
     try:
-        result = subprocess.run(['file', '-L', '--mime-type', filepath], capture_output=True, text=True)
-        mime_type = result.stdout.split(':')[1].strip()
-        return mime_type.startswith('text')
-    except Exception as e:
-        return False
-
-def check_file_for_shebang(filepath):
-    if is_text_file(filepath):
-        try:
-            with open(filepath, 'r') as file:
-                first_line = file.readline().strip()
-                if is_python_shebang(first_line):
-                    print(f"{filepath}")
-        except (IOError, OSError, UnicodeDecodeError):
-                pass
+        with open(filepath, 'rb') as file:
+            if file.read(2) == b'#!':
+                return b"python" in file.readline().strip().lower()
+    except Exception:
+        pass
+    return False
 
 def search_directory(directory):
-    for root, dirs, files in os.walk(directory):
+    for root, _, files in os.walk(directory):
         for file in files:
             filepath = os.path.join(root, file)
-            check_file_for_shebang(filepath)
+            if file_launched_with_python(filepath):
+                print(filepath)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script_name.py <dir1> <dir2> ... <dirN>")
-        sys.exit(1)
-
-    directories_to_search = sys.argv[1:]
-
+    directories_to_search = sys.argv[1:] if len(sys.argv) > 1 else os.environ['PATH'].split(':')
     for directory in directories_to_search:
         if os.path.isdir(directory):
-                search_directory(directory)
+            search_directory(directory)
